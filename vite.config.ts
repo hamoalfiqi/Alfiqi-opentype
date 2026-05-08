b88830baef07b79596193a5694554c54b6da097e
hamoalfiqi <sbry7945@gmail.com>
2026-05-08 21:24:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// ده البديل السليم لـ __dirname في نظام الـ ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [
