c20ba57a76cdb6fc874bcba3ecaf5862da1f0609
hamoalfiqi <sbry7945@gmail.com>
2026-05-08 21:29:26 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,14 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// ده البديل السليم لـ __dirname في نظام الـ ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [
-    tsConfigPaths(),
+    tsConfigPaths(), // ده لوحده هيخلي الـ @ تشتغل من غير أخطاء
     tailwindcss(),
     tanstackStart({
       target: "vercel",
     }),
     react(),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
